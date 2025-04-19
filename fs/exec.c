e2c5a79876f4a7d26b8f18f731af8237223f5a6c
manolo <manuel.gutierrez.arias2010@gmail.com>
2025-04-19 13:07:02 -0500

    3 test to se if we can speed up the compilation process

--- fs/exec.c
+++ fs/exec.c
@@ -1985,7 +1985,7 @@ static int do_execve(struct filename *filename,
 	const char __user *const __user *__argv,
 	const char __user *const __user *__envp)
 {
-	printk(KERN_ERR "MANOLO2");
+	printk(KERN_ERR "MANOLO3");
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
